8a8061fbf9e4519a816a00a6c97ef15131b3ebf2
Manjusaka <me@manjusaka.me>
2024-07-23 00:59:02 +0800

    Support + and - operator

--- nadeshiko/cmd/main.py
+++ nadeshiko/cmd/main.py
@@ -1,12 +1,24 @@
 import typer
 
 
-def main(number: int):
-    output_asm = (f"  .global main\n"
-                  f"main\n"
-                  f"  mov ${number}, %rax\n"
-                  f"  ret\n")
-    print(output_asm, flush=True)
+def main(expression: str):
+    output_asm = [f"  .global main\n",
+                  f"main:\n"]
+    assert len(expression) >= 0
+    output_asm.append(f"  mov ${expression[0]}, %rax\n")
+    index = 1
+    while index < len(expression):
+        match expression[index]:
+            case '+':
+                output_asm.append(f"  add ${expression[index + 1]}, %rax\n")
+            case '-':
+                output_asm.append(f"  sub ${expression[index + 1]}, %rax\n")
+            case _:
+                raise ValueError(f"Invalid expression: {expression}")
+        index += 2
+    output_asm.append(f"  ret\n")
+
+    print("".join(output_asm), flush=True)
 
 
 if __name__ == '__main__':
